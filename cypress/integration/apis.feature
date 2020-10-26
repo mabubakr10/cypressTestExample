1b4400fee399dd4c96d3f361faf630e4099a4544
sibghailyas967 <52412318+sibghailyas967@users.noreply.github.com>
2020-10-26 17:57:54 +0500

    Send api automation and token generation code improved

--- cypress/integration/apis.feature
+++ cypress/integration/apis.feature
@@ -1,12 +1,15 @@
 @api-testing
 Feature: Touches API automation
 
-#  Scenario: Using a valid Auth0 token and fetching all touches of a user
-#    Given I generate auth0 token
-#    Then I hit the get all touches endpoint and verify its response
+  Scenario: Using a valid Auth0 token and fetching all touches of a user
+    Given I generate auth0 token
+    Then I hit the get all touches endpoint and verify its response
 
-
-  Scenario: Sending a touch via send API
+  Scenario: Sending an eGift touch via send API
     Given I generate auth0 token
     Then I send a touch and verify response
+
+  Scenario: Sending a physical touch via send API
+    Given I generate auth0 token
+    Then I send a physical touch and verify response
 
--- cypress/support/step_definitions/api_testing_steps.js
+++ cypress/support/step_definitions/api_testing_steps.js
@@ -4,12 +4,15 @@ import APIactions from './steps/APIactions'
 Given(/^I generate auth0 token$/, function() {
     APIactions.getAuthAccessToken(Cypress.env('auth_username'), Cypress.env('auth_password'),
       Cypress.env('auth_redirect_uri'), Cypress.env('auth_client_id'),
-      Cypress.env('auth_client_secret') , "core-qa");
+      Cypress.env('auth_client_secret') , Cypress.env('instance'));
 });
 
 Then(/^I hit the get all touches endpoint and verify its response$/, function () {
-    APIactions.getTouches("core-qa");
+    APIactions.getTouches(Cypress.env('instance'));
 });
 Then(/^I send a touch and verify response$/, function () {
-    APIactions.sendTouch("61383", "core-qa")
+    APIactions.sendTouch("61383", Cypress.env('instance'))
+});
+Then(/^I send a physical touch and verify response$/, function () {
+    APIactions.sendPhysicalGift("61310", Cypress.env('instance'));
 });
