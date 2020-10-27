8a09a6f396882fd73c46c4f3a9094c629f64dd8f
sibghailyas967 <52412318+sibghailyas967@users.noreply.github.com>
2020-10-27 20:05:42 +0500

    Databse connection function added

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,17 +12,37 @@
 
 const AllureWriter = require('@shelex/cypress-allure-plugin/writer');
 const cucumber = require('cypress-cucumber-preprocessor').default
+const mysql = require('mysql')
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  on('task', {
+    // Usage: cy.task('queryDb', query)
+    queryDb: query => {
+      return queryTestDb(query, config)
+    },
+  })
+
   on('file:preprocessor', cucumber())
   AllureWriter(on, config);
   return config;
 }
-
-afterEach(() => {
-  // runs after each test in the block
-})
-
+function queryTestDb(query, config) {
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db)
+  // start connection to db
+  connection.connect()
+  // exec query + disconnect to db as a Promise
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) reject(error)
+      else {
+        connection.end()
+        // console.log(results)
+        return resolve(results)
+      }
+    })
+  })
+}
