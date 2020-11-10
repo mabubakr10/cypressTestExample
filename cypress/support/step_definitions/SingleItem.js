026b09fedf44abb8cd2869470f9d73474d5f154f
jaffarlone <jaffar.lone@sendoso.com>
2020-11-10 18:16:50 +0500

    Updated changes as asked in the review

--- cypress/support/step_definitions/SingleItem.js
+++ cypress/support/step_definitions/SingleItem.js
@@ -2,32 +2,22 @@ import { Given, When, Then, And,Before } from 'cypress-cucumber-preprocessor/ste
 import LoginPage from "./steps/loginPage";
 import FP1046_Single_item_send_steps from "./steps/FP1046_Single_item_send_steps";
 
+
 Then(/^I click on the Send button$/, function () {
-    { force: true }
-    cy.wait(3000)
-    // cy.visit("https://core-qa.sendoso.com/send#/")
-   cy.get('.send-action').click()
+    FP1046_Single_item_send_steps.clickSendBtn();
 });
 Then(/^I click on the single item touch$/, function () {
-    { force: true }
-    cy.wait(2000)
-    cy.xpath("//div[@class='col-sm-4 egift_radio_btn_div'][1]").click()
+    FP1046_Single_item_send_steps.clickSingleItemtouch()
 });
 Given(/^I close up the pop up button after login$/, function () {
     cy.wait(3000)
     LoginPage.closePopUp()
 });
 Then(/^I signout of the application$/, function () {
-    { force: true }
-    cy.wait(2000)
-    cy.xpath("//*[@id=\"global_nav_sidebar\"]/div/div[4]/nav/ul/li[3]/a").click()
-    cy.wait(2000)
-    cy.xpath("//*[@id=\"global_nav_sidebar\"]/div/div[4]/nav/ul/li[3]/div/a[2]").click()
+    FP1046_Single_item_send_steps.signout()
 });
 Then(/^I select single send method of sending$/, function () {
-    { force: true }
-    cy.wait(2000)
-    cy.get('.container > .row > .col-md-4 > .form-group > #send_option').select('4')
+    FP1046_Single_item_send_steps.selectsingleitemsend()
 });
 Then(/^I add mailing address$/, function () {
     FP1046_Single_item_send_steps.AddressPick()
